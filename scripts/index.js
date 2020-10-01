041ac15c957be99e4774b11b7c2d4013db46d9f7
Daniel Zherenkov <daniil4people@gmail.com>
2020-10-01 21:20:16 +0300

    правки от 1.10.20

--- scripts/index.js
+++ scripts/index.js
@@ -1,32 +1,30 @@
 let editButton = document.querySelector('.profile__edit-button');
 let popup = document.querySelector('.popup');
 let popupCloseButton = document.querySelector('.popup__close-button');
-let popupNameFiled = document.querySelector('.popup__name-filed');
-let popupAboutFiled = document.querySelector('.popup__about-filed');
+let popupNameFiled = document.querySelector('.popup__input-filed');
+let popupAboutFiled = document.querySelector('.popup__input-filed_about');
 let profileName = document.querySelector('.profile__name');
 let profileAbout = document.querySelector('.profile__about-self');
+let formElement = document.querySelector('.popup__container'); 
 
 function openPopup() {
     popup.classList.add('popup_opened');
+    popupNameFiled.value = profileName.textContent;
+    popupAboutFiled.value = profileAbout.textContent;
 }
 
 function closePopup() {
     popup.classList.remove('popup_opened');
 }
 
-editButton.addEventListener('click', openPopup);
-popupCloseButton.addEventListener('click', closePopup);
-
-popupNameFiled.value = profileName.textContent;
-popupAboutFiled.value = profileAbout.textContent;
-
-let formElement = document.querySelector('.popup__container'); 
 function formSubmitHandler (evt) {
     evt.preventDefault(); 
-    profileName.textContent = popupNameFiled.value 
-    profileAbout.textContent = popupAboutFiled.value
-    closePopup()
+    profileName.textContent = popupNameFiled.value;
+    profileAbout.textContent = popupAboutFiled.value;
+    closePopup();
     
 }
 
+editButton.addEventListener('click', openPopup);
+popupCloseButton.addEventListener('click', closePopup);
 formElement.addEventListener('submit', formSubmitHandler); 
